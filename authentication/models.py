c674e2567cd3802faf763a88d262d20fa39daaab
Barkhayot Juraev <coderviewer@gmail.com>
2022-03-09 22:37:38 +0900

    files

---
+++ authentication/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from helpers.models import TrackingModel
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils import timezone 
+from django.contrib.auth.models import (AbstractBaseUser, UserManager, PermissionsMixin)
+from django.utils.translation import gettext_lazy as _
+
+class MyUserManager(UserManager):
+
+    def _create_user(self, username, email, password, **extra_fields):
+         
+        if not username:
+            raise ValueError('The given username must be set')
+
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(TrackingModel, AbstractBaseUser, PermissionsMixin):
+    
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'), 
+        max_length=150, 
+        unique=True, 
+        help_text=_("Required. 150 charecters only!"), 
+        validators=[username_validator], 
+        error_messages={'unique': _("A user with that username already exists")},)
+    email = models.EmailField(_("email address"), blank=False, unique=True)
+    is_stuff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("User can be logged in as the stuff")
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_("Active status of user")
+    )
+    email_verified = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_("Email status of user")
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = MyUserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    @property
+    def token(self):
+        return ''
